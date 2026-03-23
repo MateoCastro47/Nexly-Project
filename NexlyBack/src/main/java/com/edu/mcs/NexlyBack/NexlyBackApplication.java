e0d6104d945ff2e6d38d603a4eefb8012cf3fd67
MateoCastro47 <castrosoutomateo@gmail.com>
2026-03-23 19:32:29 +0100

    feat: Creación proyecto spring para el back

---
+++ NexlyBack/src/main/java/com/edu/mcs/NexlyBack/NexlyBackApplication.java
@@ -0,0 +1,13 @@
+package com.edu.mcs.NexlyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexlyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexlyBackApplication.class, args);
+	}
+
+}
---
+++ NexlyBack/src/test/java/com/edu/mcs/NexlyBack/NexlyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.mcs.NexlyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexlyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
