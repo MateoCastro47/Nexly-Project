93e37240f4bb6ff759147b43a508475484b9f9d1
MateoCastro47 <castrosoutomateo@gmail.com>
2026-04-29 13:20:59 +0200

    feat(imagenes): configurar Cloudinary con CloudinaryConfig

---
+++ NexlyBack/src/main/java/com/edu/mcs/NexlyBack/Config/CloudinaryConfig.java
@@ -0,0 +1,15 @@
+package com.edu.mcs.NexlyBack.Config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary cloudinary(@Value("${CLOUDINARY_URL}") String cloudinaryUrl) {
+        return new Cloudinary(cloudinaryUrl);
+    }
+}
